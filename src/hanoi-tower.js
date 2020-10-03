869bab570d8b7595902de0ebf4a206b8f04d566b
Nikolaj Lozitski <Soullnik96@gmail.com>
2020-10-03 14:15:57 +0300

    feat: hanoi-tower is completed

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+  const result = {
+    turns: 0,
+    seconds: 0
+  }
+
+  const towerOfHanoi = (n, source = 'A',auxiliary = 'B', destination = 'C') => {
+    if (n === 1) {
+      result.turns += 1;
+    } else {
+      towerOfHanoi(n - 1, source, destination, auxiliary);
+      towerOfHanoi(n - 1, auxiliary, source, destination);
+      result.turns +=1
+    }
+  }
+
+  towerOfHanoi(disksNumber)
+  result.seconds =  Math.floor(result.turns / (turnsSpeed / 3600))
+  return  result
+}
